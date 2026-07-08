055c34d3ebec23bcfa208e132746454d05cc6283
John Gietzen <john@gietzen.us>
2026-07-08 13:48:37 -0700

    Added Zip archive editing.

--- Codec.Base/Archives/ZipArchiveFileSystem.cs
+++ Codec.Base/Archives/ZipArchiveFileSystem.cs
@@ -27,15 +27,28 @@
 
         protected override Stream Open(ZipArchiveEntry entry, FileStreamOptions parentOptions)
         {
-            if (parentOptions.Mode != FileMode.Open)
-            {
-                throw new NotSupportedException("ZipArchiveFileSystem currently only supports opening files in read-only mode.");
-            }
+            return CreateStreamWrapper(
+                parentOptions,
+                options =>
+                {
+                    var file = parent.File.Open(parentRelativePath, options);
+                    var archive = new ZipArchive(file, ZipArchiveMode.Read);
+                    entry = archive.GetEntry(entry.FullName)!;
+                    return new CachingSeekableStream(new DisposingStream(entry.Open(), archive));
+                },
+                updated =>
+                {
+                    using var file = parent.File.Open(parentRelativePath, FileMode.Open, FileAccess.ReadWrite);
+                    using var archive = new ZipArchive(file, ZipArchiveMode.Update);
 
-            var file = parent.File.OpenRead(parentRelativePath);
-            var archive = new ZipArchive(file, ZipArchiveMode.Read);
-            entry = archive.GetEntry(entry.FullName)!;
-            return new CachingSeekableStream(new DisposingStream(entry.Open(), archive));
+                    var zipEntry = archive.GetEntry(entry.FullName)
+                        ?? throw new FileNotFoundException($"Entry '{entry.FullName}' not found.");
+
+                    using var destination = zipEntry.Open();
+                    destination.SetLength(0);
+                    updated.Position = 0;
+                    updated.CopyTo(destination);
+                });
         }
 
         protected override IEnumerable<ZipArchiveEntry> ReadIndex()
