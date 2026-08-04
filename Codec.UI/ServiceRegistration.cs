a0cf5317938c98221966d72c26626e0e52cecb6c
John Gietzen <john@gietzen.us>
2026-08-04 12:21:18 -0700

    Move logging to Serilog.

--- Codec.UI/ServiceRegistration.cs
+++ Codec.UI/ServiceRegistration.cs
@@ -2,13 +2,15 @@
 
 namespace Codec.UI
 {
+    using System;
     using System.CommandLine;
     using System.CommandLine.Invocation;
     using System.Diagnostics;
+    using System.IO;
     using Codec.Services;
     using Microsoft.Extensions.DependencyInjection;
-    using Microsoft.Extensions.DependencyInjection.Extensions;
     using Microsoft.Extensions.Logging;
+    using Serilog;
 
     public class ServiceRegistration
     {
@@ -25,9 +27,24 @@ namespace Codec.UI
 
         public static void Register(InvocationContext context, IServiceCollection services)
         {
+            Log.Logger = new LoggerConfiguration()
+#if DEBUG
+                .MinimumLevel.Debug()
+#else
+                .MinimumLevel.Warning()
+#endif
+                .WriteTo.Console()
+                .WriteTo.File(
+                    Path.ChangeExtension(Environment.ProcessPath!, ".log"),
+                    fileSizeLimitBytes: 5 * 1024 * 1024, // 5 MB
+                    rollOnFileSizeLimit: true,
+                    retainedFileCountLimit: 5,
+                    shared: true)
+                .CreateLogger();
+
             services.AddLogging(builder =>
             {
-                builder.AddConsole();
+                builder.AddSerilog();
 
                 var notifier = new NotifyingLoggerProvider();
                 builder.Services.AddSingleton(notifier);
