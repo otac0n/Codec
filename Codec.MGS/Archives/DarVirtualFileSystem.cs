c80a93254e75d2e03ab34d8fc12300e2d7ea55b3
John Gietzen <john@gietzen.us>
2026-08-12 20:42:53 -0700

    Added support for aligned DAR files (PSP games)

--- Codec.MGS/Archives/DarVirtualFileSystem.cs
+++ Codec.MGS/Archives/DarVirtualFileSystem.cs
@@ -34,6 +34,7 @@ namespace Codec.MGS.Archives
         {
             using var source = parent.File.OpenRead(parentRelativePath);
             var fileCount = source.ReadUInt32LittleEndian();
+            bool? alignData = null;
             for (var i = 0; i < fileCount; i++)
             {
                 var nameBuilder = new StringBuilder();
@@ -52,8 +53,41 @@ namespace Codec.MGS.Archives
 
                 var name = nameBuilder.ToString();
                 var length = source.ReadUInt32LittleEndian();
+
+                ApplyAlignment(source, length, ref alignData);
+
                 yield return (name, source.Position, length);
                 source.Position += length + 1;
+            }
+        }
+
+        private static void ApplyAlignment(FileSystemStream source, uint length, ref bool? alignData)
+        {
+            const int Alignment = 16;
+            if (alignData is bool value)
+            {
+                if (value)
+                {
+                    source.Align(Alignment);
+                }
+
+                return;
+            }
+
+            var padding = StreamExtensions.GetPadding(source.Position, Alignment);
+            if (padding > 0)
+            {
+                if (source.Position + padding + length >= source.Length || !source.PeekAllZeros(padding))
+                {
+                    alignData = false;
+                    return;
+                }
+
+                var position = source.Position;
+                source.Position += padding + length;
+                (source.Position, alignData) = source.ReadByte() == 0
+                        ? (position + padding, true)
+                        : (position, false);
             }
         }
 
