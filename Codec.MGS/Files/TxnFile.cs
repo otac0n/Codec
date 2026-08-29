77a0260b3a52425463f73b30885f7b753544680f
John Gietzen <john@gietzen.us>
2026-08-29 00:05:52 -0700

    Expose texture atlas images in TXN textures.

--- Codec.MGS/Files/TxnFile.cs
+++ Codec.MGS/Files/TxnFile.cs
@@ -5,17 +5,17 @@ namespace Codec.MGS.Files
     using System;
     using System.Collections.Generic;
     using System.IO;
-    using System.IO.Abstractions;
     using System.Linq;
     using System.Runtime.InteropServices;
     using Codec;
     using Codec.Archives;
     using Codec.Imaging;
     using DiscUtils.Streams;
+    using ImageMagick;
     using Microsoft.Extensions.DependencyInjection;
     using static Codec.Imaging.DdsConstants;
 
-    internal class TxnFile
+    public class TxnFile
     {
         public static void Register(IServiceCollection services)
         {
@@ -30,73 +30,109 @@ namespace Codec.MGS.Files
             });
         }
 
-        private class TxnFileFileSystem(string fullPath, NestedFileSystemManager fsm) : IndexedFileSystem<uint>
+        public static (TxnHeader Header, TxnImage[] Images, TxnTexture[] Textures) ReadHeaders(Stream stream)
         {
-            protected override IEnumerable<uint> ReadIndex()
+            var header = stream.ReadBigEndian<TxnHeader>();
+            stream.Position = header.ImageOffset;
+            var images = stream.ReadArrayBigEndian<TxnImage>((int)header.ImageCount);
+            stream.Position = header.TextureOffset;
+            var textureDefinitions = stream.ReadArrayBigEndian<TxnTexture>((int)header.TextureCount);
+            return (header, images, textureDefinitions);
+        }
+
+        private static int GetImageIndex(TxnHeader header, TxnTexture tx)
+        {
+            return (int)((tx.ImageOffset - header.ImageOffset) / Marshal.SizeOf<TxnImage>());
+        }
+
+        public static IEnumerable<Entry> FindRelatedFiles(string fullPath, NestedFileSystemManager fsm, uint strCode, int ix)
+        {
+            var folderName = PathExtensions.GetDirectoryName(fullPath) ?? string.Empty;
+            var searchPaths = new List<string>
+            {
+                folderName,
+            };
+
+            if (!string.IsNullOrEmpty(folderName))
+            {
+                folderName = PathExtensions.GetDirectoryName(folderName) ?? string.Empty;
+                searchPaths.Add(folderName);
+            }
+
+            var targetName = $"{strCode:x8}_{ix}.data";
+            return searchPaths.SelectMany(searchPath => fsm.EnumerateFiles(searchPath, targetName, recursive: true));
+        }
+
+        private class TxnFileFileSystem(string fullPath, NestedFileSystemManager fsm) : IndexedFileSystem<int>
+        {
+            protected override IEnumerable<int> ReadIndex()
             {
                 using var stream = fsm.OpenRead(fullPath);
-                var header = stream.ReadBigEndian<TxnHeader>();
-                stream.Position = header.TextureOffset;
-                var textureDefinitions = stream.ReadArrayBigEndian<TxnTexture>((int)header.TextureCount);
-                var ids = new List<uint>((int)header.TextureCount);
-                for (var t = 0; t < header.TextureCount; t++)
+                var (header, images, textures) = ReadHeaders(stream);
+
+                var entries = new List<int>();
+                for (var i = 0; i < images.Length; i++)
                 {
-                    var info = textureDefinitions[t];
-                    stream.Position = info.ImageOffset;
-                    var image = stream.ReadBigEndian<TxnImage>();
-                    if (image.PixelFormat is 0x08 or 0x09 or 0x0B)
+                    entries.Add(~i);
+                }
+
+                foreach (var tx in textures)
+                {
+                    entries.Add((int)tx.Id);
+                    if (!(tx.UOffset != 0 || tx.VOffset != 0 || tx.UScale != 1 || tx.VScale != 1))
                     {
-                        ids.Add(info.Id);
+                        entries.Remove(~GetImageIndex(header, tx));
                     }
                 }
 
-                return ids;
+                return entries;
             }
 
-            protected override string GetEntryName(uint entry) =>
-                $"{entry:x6}.dds";
+            protected override string GetEntryName(int entry) =>
+                entry < 0 ? $"{~entry}.dds" : $"{entry:x6}.dds";
 
-            protected override Stream Open(uint entry, FileStreamOptions parentOptions)
+            protected override Stream Open(int entry, FileStreamOptions parentOptions)
             {
                 FileBase.EnsureReadOnly(parentOptions, "Writing to sub images in .txn files is not supported.");
 
                 var stream = fsm.Open(fullPath, parentOptions);
-                var header = stream.ReadBigEndian<TxnHeader>();
-                stream.Position = header.TextureOffset;
-                var textureDefinitions = stream.ReadArrayBigEndian<TxnTexture>((int)header.TextureCount);
-                var ix = Array.FindIndex(textureDefinitions, t => t.Id == entry);
-                if (ix == -1)
+                var (header, images, textures) = ReadHeaders(stream);
+
+                int ix;
+                TxnTexture? texture = null;
+                if (entry < 0)
                 {
-                    throw new FileNotFoundException($"Texture with ID '{entry:x8}' not found.");
+                    ix = ~entry;
+                }
+                else
+                {
+                    ix = Array.FindIndex(textures, t => t.Id == entry);
+                    if (ix < 0)
+                    {
+                        throw new FileNotFoundException($"Texture with ID '{entry:x8}' not found.");
+                    }
+
+                    texture = textures[ix];
+                    ix = GetImageIndex(header, textures[ix]);
                 }
 
-                var texture = textureDefinitions[ix];
-                stream.Position = texture.ImageOffset;
-                var image = stream.ReadBigEndian<TxnImage>();
+                if (ix < 0 || ix >= images.Length)
+                {
+                    throw new FileNotFoundException($"Image with Index '{ix}' not found.");
+                }
+
+                var image = images[ix];
 
                 Stream dataStream;
                 var external = (image.Flags & 0xF0) == 0xF0;
                 if (external)
                 {
                     stream.Dispose();
-                    var folderName = PathExtensions.GetDirectoryName(fullPath) ?? string.Empty;
-                    var searchPaths = new List<string>
-                    {
-                        folderName,
-                    };
 
-                    if (!string.IsNullOrEmpty(folderName))
-                    {
-                        folderName = PathExtensions.GetDirectoryName(folderName) ?? string.Empty;
-                        searchPaths.Add(folderName);
-                    }
-
-                    var targetName = $"{texture.FileId:x8}_{ix}.data";
-
-                    var found = searchPaths.SelectMany(searchPath => fsm.EnumerateFiles(searchPath, targetName, recursive: true)).FirstOrDefault(f => f is not null);
+                    var found = FindRelatedFiles(fullPath, fsm, (texture ?? textures[0]).FileId, ix).FirstOrDefault(f => f is not null);
                     if (found is not { CanOpen: true })
                     {
-                        throw new FileNotFoundException($"External texture file '{targetName}' not found.");
+                        throw new FileNotFoundException($"External texture file not found.");
                     }
 
                     dataStream = fsm.OpenRead(found.Path);
@@ -108,48 +144,98 @@ namespace Codec.MGS.Files
                     dataStream = new OffsetStreamSpan(stream, dataOffset, dataLength, Ownership.Dispose);
                 }
 
-                return BuildDdsStream(image, dataStream);
+                var dds = BuildDdsStream(image, dataStream);
+                if (texture is TxnTexture tx && (tx.UOffset != 0 || tx.VOffset != 0 || tx.UScale != 1 || tx.VScale != 1))
+                {
+                    var ms = new MemoryStream();
+                    using (dds)
+                    using (var collection = new MagickImageCollection())
+                    {
+                        collection.Read(dds, MagickFormat.Dds);
+
+                        var x = (int)Math.Round(tx.UOffset * image.Width);
+                        var y = (int)Math.Round(tx.VOffset * image.Height);
+                        for (var i = 0; i < collection.Count; i++)
+                        {
+                            var mip = collection[i];
+                            var mipWidth = (int)mip.Width;
+                            var mipHeight = (int)mip.Height;
+
+                            var scaleX = (double)mipWidth / image.Width;
+                            var scaleY = (double)mipHeight / image.Width;
+
+                            var mipX = (int)Math.Round(x * scaleX);
+                            var mipY = (int)Math.Round(y * scaleY);
+                            var mipCropWidth = Math.Max(1, (int)Math.Round(tx.Width * scaleX));
+                            var mipCropHeight = Math.Max(1, (int)Math.Round(tx.Height * scaleY));
+
+                            mipCropWidth = Math.Min(mipCropWidth, mipWidth - mipX);
+                            mipCropHeight = Math.Min(mipCropHeight, mipHeight - mipY);
+
+                            mip.Crop(new MagickGeometry(mipX, mipY, (uint)mipCropWidth, (uint)mipCropHeight));
+                            mip.ResetPage();
+                        }
+
+                        collection.Write(ms, MagickFormat.Dds);
+                        ms.Position = 0;
+                        return ms;
+                    }
+                }
+                else
+                {
+                    return dds;
+                }
             }
 
             private static Stream BuildDdsStream(TxnImage image, Stream dataStream)
             {
                 var sizeOfHeader = Marshal.SizeOf<DDS_HEADER>();
                 var sizeOfFormat = (uint)Marshal.SizeOf<DDS_PIXELFORMAT>();
-                var blockSize = image.PixelFormat == 0x09 ? 8u : 16u;
-                var pixelFormat = image.PixelFormat switch
+                var (blockSize, pixelFormat) = image.PixelFormat switch
                 {
-                    // TODO: 0x03 => ???
-                    0x08 => new DDS_PIXELFORMAT
+                    0x03 or 0x08 => (0, new DDS_PIXELFORMAT
                     {
                         Size = sizeOfFormat,
                         Flags = DDPF_RGB | DDPF_ALPHA,
+                        ABitMask = 0xFF000000,
                         RBitMask = 0x00FF0000,
                         GBitMask = 0x0000FF00,
                         BBitMask = 0x000000FF,
-                        ABitMask = 0xFF000000,
-                    },
-                    0x09 => new DDS_PIXELFORMAT
+                    }),
+                    0x09 => (8, new DDS_PIXELFORMAT
                     {
                         Size = sizeOfFormat,
                         Flags = DDPF_FOURCC,
                         FourCC = 0x31545844, // 'DXT1'
-                    },
-                    0x0B => new DDS_PIXELFORMAT
+                    }),
+                    0x0A => (16, new DDS_PIXELFORMAT
+                    {
+                        Size = sizeOfFormat,
+                        Flags = DDPF_FOURCC,
+                        FourCC = 0x33545844, // 'DXT3'
+                    }),
+                    0x0B => (16, new DDS_PIXELFORMAT
                     {
                         Size = sizeOfFormat,
                         Flags = DDPF_FOURCC,
                         FourCC = 0x35545844, // 'DXT5'
-                    },
+                    }),
                 };
+
+                var width = image.Width;
+                var height = image.Height;
+
                 var dds = new DDS_HEADER
                 {
                     Signature = 0x20534444u,
                     Size = (uint)sizeOfHeader - 4, // Signature not included in size.
                     Flags = DDSD_CAPS | DDSD_HEIGHT | DDSD_WIDTH | DDSD_PIXELFORMAT | DDSD_LINEARSIZE,
                     Caps1 = DDSCAPS_TEXTURE,
-                    Width = image.Width,
-                    Height = image.Height,
-                    PitchOrLinearSize = Math.Max(1u, ((uint)image.Width + 3) / 4) * Math.Max(1u, ((uint)image.Height + 3) / 4) * blockSize,
+                    Width = width,
+                    Height = height,
+                    PitchOrLinearSize = blockSize == 0
+                        ? (uint)dataStream.Length
+                        : (uint)(Math.Max(1, (width + 3) / 4) * Math.Max(1, (height + 3) / 4) * blockSize),
                     Depth = 0u,
                     PixelFormat = pixelFormat,
                 };
